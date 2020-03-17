2332689e8e1dbc6f5ad6e3bdea33d6bf4ab38101
Martin Hunt <martin.hunt@adarga.ai>
2020-03-17 17:23:56 +0000

    turn off fuzzy links

--- src/decorators/defaultMatchDecorator.js
+++ src/decorators/defaultMatchDecorator.js
@@ -5,6 +5,7 @@ import tlds from 'tlds';
 
 const linkify = new LinkifyIt();
 linkify.tlds(tlds);
+linkify.set({ fuzzyLink: false });
 
 export default (text: string): Array<Object> => {
   return linkify.match(text);
